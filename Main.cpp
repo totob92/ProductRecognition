420910b0e1da774f304220f04aed318409a7cf6f
arma22 <armando.mangiocco.22@outlook.com>
2017-01-24 13:32:43 +0100

    Test 4 model

--- Main.cpp
+++ Main.cpp
@@ -12,9 +12,10 @@ int main(int argc, char**argv){
 
 	
 	cv::Mat model_1 = cv::imread("C:/Users/arman/Desktop/Progetto_CV/models/0.jpg");
-	cv::Mat model_2 = cv::imread("C:/Users/arman/Desktop/Progetto_CV/models/1.jpg");
-	cv::Mat model_3 = cv::imread("C:/Users/arman/Desktop/Progetto_CV/models/11.jpg");
-	cv::Mat scene = cv::imread("C:/Users/arman/Desktop/Progetto_CV/scenes/e3.png");
+	cv::Mat model_2 = cv::imread("C:/Users/arman/Desktop/Progetto_CV/models/26.jpg");
+	cv::Mat model_3 = cv::imread("C:/Users/arman/Desktop/Progetto_CV/models/25.jpg");
+	cv::Mat model_4 = cv::imread("C:/Users/arman/Desktop/Progetto_CV/models/11.jpg");
+	cv::Mat scene = cv::imread("C:/Users/arman/Desktop/Progetto_CV/scenes/e4.png");
 	if (model_1.data == NULL || scene.data == NULL){
 		std::cout << "Unable to load image..." << std::endl;
 		std::exit(1);
@@ -31,30 +32,33 @@ int main(int argc, char**argv){
 		cv::Ptr<cv::Feature2D> descriptor = cv::BRISK::create();
 	#endif
 
-	std::vector<cv::KeyPoint> keypoints_model_1, keypoints_model_2, keypoints_model_3, keypoints_scene;
+		std::vector<cv::KeyPoint> keypoints_model_1, keypoints_model_2, keypoints_model_3, keypoints_model_4, keypoints_scene;
 	
 	Keypoint_Detection(model_1, detector, keypoints_model_1);
 	Keypoint_Detection(model_2, detector, keypoints_model_2);
 	Keypoint_Detection(model_3, detector, keypoints_model_3);
+	Keypoint_Detection(model_4, detector, keypoints_model_4);
 	Keypoint_Detection(scene, detector, keypoints_scene);
 
-	cv::Mat descriptor_model_1, descriptor_model_2, descriptor_model_3, descriptor_scene;
+	cv::Mat descriptor_model_1, descriptor_model_2, descriptor_model_3, descriptor_model_4, descriptor_scene;
 
 	Keypoint_Descriptor(model_1, keypoints_model_1, descriptor, descriptor_model_1);
 	Keypoint_Descriptor(model_2, keypoints_model_2, descriptor, descriptor_model_2);
 	Keypoint_Descriptor(model_3, keypoints_model_3, descriptor, descriptor_model_3);
+	Keypoint_Descriptor(model_4, keypoints_model_4, descriptor, descriptor_model_4);
 	Keypoint_Descriptor(scene, keypoints_scene, descriptor, descriptor_scene);
 
 	#if (defined(CV_VERSION_EPOCH) && CV_VERSION_EPOCH == 2)
 		cv::Ptr<cv::DescriptorMatcher> matcher_1 = cv::DescriptorMatcher::create("FlannBased");
 		cv::Ptr<cv::DescriptorMatcher> matcher_2 = cv::DescriptorMatcher::create("FlannBased");
 		cv::Ptr<cv::DescriptorMatcher> matcher_3 = cv::DescriptorMatcher::create("FlannBased");
+		cv::Ptr<cv::DescriptorMatcher> matcher_4 = cv::DescriptorMatcher::create("FlannBased");
 	#else
 		cv::Ptr<cv::DescriptorMatcher> matcher = cv::makePtr<cv::FlannBasedMatcher>(new cv::flann::LshIndexParams(10, 20, 2));
 	#endif
 
-		std::vector<cv::DMatch> filtered_match_1, filtered_match_2, filtered_match_3;
-		std::vector<cv::DMatch> all_match_1, all_match_2, all_match_3;
+		std::vector<cv::DMatch> filtered_match_1, filtered_match_2, filtered_match_3, filtered_match_4;
+		std::vector<cv::DMatch> all_match_1, all_match_2, all_match_3, all_match_4;
 	
 	Descriptor_Matching(model_1, scene, descriptor_model_1, descriptor_scene, keypoints_model_1, keypoints_scene, matcher_1, filtered_match_1, all_match_1);
 	
@@ -62,6 +66,7 @@ int main(int argc, char**argv){
 	
 	Descriptor_Matching(model_3, scene, descriptor_model_3, descriptor_scene, keypoints_model_3, keypoints_scene, matcher_3, filtered_match_3, all_match_3);
 	
+	Descriptor_Matching(model_4, scene, descriptor_model_4, descriptor_scene, keypoints_model_4, keypoints_scene, matcher_4, filtered_match_4, all_match_4);
 
 	//Homography(model_1, scene, keypoints_model_1, keypoints_scene, filtered_match_1);
 	
@@ -71,9 +76,9 @@ int main(int argc, char**argv){
 	
 	
 	
-	std::vector<cv::Mat> models = { model_1, model_2, model_3 };
-	std::vector<std::vector<cv::DMatch>> filtered_matches = { filtered_match_1, filtered_match_2, filtered_match_3 };
-	std::vector<std::vector<cv::KeyPoint>> keypoints_models = { keypoints_model_1, keypoints_model_2, keypoints_model_3 };
+	std::vector<cv::Mat> models = { model_1, model_2, model_3, model_4 };
+	std::vector<std::vector<cv::DMatch>> filtered_matches = { filtered_match_1, filtered_match_2, filtered_match_3, filtered_match_4 };
+	std::vector<std::vector<cv::KeyPoint>> keypoints_models = { keypoints_model_1, keypoints_model_2, keypoints_model_3, keypoints_model_4 };
 	
 	Multi_Homography(scene, keypoints_scene, models, filtered_matches, keypoints_models);
 
